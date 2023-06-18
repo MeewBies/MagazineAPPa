3090953e7ab3d204b9b93ae7dfeedf1660189a4d
untermench <stalkerpro200306@gmail.com>
2023-06-18 17:28:56 +0500

    БД

---
+++ MagazineAPP/DB/c.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagazineAPP.DB
+{
+    class c
+    {
+        public static fishMarketEntities con = new fishMarketEntities();
+    }
+}
